dfb03e05d515a2fe2ee5f95f64c688f440ab9442
박성빈 <tallbin98@gmail.com>
2019-06-02 16:24:41 +0900

    DB init

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 # _*_ coding: utf-8 _*_
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from flask_socketio import SocketIO, emit
+import sqlite3
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+DATABASE = 'test.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(Exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def hello_world():
