daa926f7662b467f4361b9e240edebfbf10bcaaa
박성빈 <tallbin98@gmail.com>
2019-06-07 16:52:54 +0900

    create user

--- app.py
+++ app.py
@@ -19,6 +19,16 @@ def get_login(userid, pwd):
     else:
         return False
 
+    
+def check_db(db_name):
+    db = sqlite3.connect("test.db")
+    cur = db.cursor()
+    flag = cur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='?';", db_name)
+    if flag == 1:
+        return True
+    else:
+        return False
+    
 @app.route('/')
 def index():
     if session.get("account_id") is not None:
@@ -32,10 +42,11 @@ def login():
     user_id = request.form["user_id"]
     user_pw = request.form["user_pw"]
 
+    db = sqlite3.connect("test.db")
+    cur = db.cursor()
     '''
     @TODO : db connect and compare user id and pw
     '''
-    if(get_login(user_id, user_pw) == True):
     # session['account_id'] = 
     '''
     @TODO : session create with db
@@ -66,12 +77,13 @@ def create():
     user_pw = request.form["user_pw"]
     user_em = request.form["user.email"]
     user_name = request.form["user_name"]
-
-    '''
-    @TODO : 디비에 user 추가
-    '''
-
-    return
+    db = sqlite3.connect()
+    cur = db.cursor()
+    cur.execute("INSERT INTO user(userid, pwd, email, name) VALUES(?, ?, ?, ?)", user_id, user_pw, user_em, user_name)
+    cur.commit()
+    cur.close()
+    db.close()
+    return redirect('/', code=302)
 
 #app start
 if __name__ == '__main__':
